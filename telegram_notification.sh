9b534c0ba2c331aa212c07a39d0da76882709853
Rafael Loureiro <30809327+loureiroraf@users.noreply.github.com>
2022-04-17 22:42:34 -0300

    Update telegram_notification.sh

--- telegram_notification.sh
+++ telegram_notification.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Get the token from Travis environment vars and build the bot URL:
-BOT_URL="https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage"
+BOT_URL="https://api.telegram.org/bot5136919477:AAHrKi4BlZIBcSxUlbzmrvgeY94EvviYDYg/sendMessage"
 
 # Set formatting for the message. Can be either "Markdown" or "HTML"
 PARSE_MODE="Markdown"
